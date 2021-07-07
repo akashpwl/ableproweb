fe8fc8dc86599bc47a12c8a1edaf7fb63b149b95
Akash <akash@newput.com>
2021-07-07 20:49:33 +0530

    ablepro web

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/ablepro-deploy'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res)=> {
-  res.sendFile('index.html', {root: 'dist/ablepro-deploy/'})
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
